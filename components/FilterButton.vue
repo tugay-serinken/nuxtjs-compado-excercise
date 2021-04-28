d99d09f7d6833fdeea52d97b597601bcf016e63b
Tugay Serinken <tugay.serinken@hotmail.de>
2021-04-28 23:53:31 +0700

    format

--- components/FilterButton.vue
+++ components/FilterButton.vue
@@ -1,8 +1,8 @@
 <template>
-  <div className="filter-button">
+  <div class="filter-button">
     <p>Hide</p>
     <FilterIcon
-      className="filter-icon"
+      class="filter-icon"
       name="sorting_options"
       style="height:10px; width:10px"
     />
@@ -10,31 +10,31 @@
 </template>
 
 <script lang="ts">
-import FilterIcon from "./Icon.vue";
+import FilterIcon from './Icon.vue'
 
 export default {
   components: { FilterIcon }
-};
+}
 </script>
 
 <style lang="sass" scoped>
 .filter-button
-    display: flex
-    justify-content: center
-    align-items: center
-    height: 30px
-    padding-right: 50px
-    @media (max-width: 1400px)
-        p
-            display: none
+  display: flex
+  justify-content: center
+  align-items: center
+  height: 30px
+  padding-right: 50px
+  @media (max-width: 1400px)
+      p
+          display: none
 
-    p
-        font-size: 16px
-        padding-right: 8px
+  p
+      font-size: 16px
+      padding-right: 8px
 
-    &:hover
-        cursor: pointer
+  &:hover
+      cursor: pointer
 
 .filter-icon
-    display: flex
+  display: flex
 </style>
