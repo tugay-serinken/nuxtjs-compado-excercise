7456aed984fd299d9b68c8f29f1d6585f160770f
Phạm Minh Tiến <phaminhtien2424@gmail.com>
2021-05-06 12:37:02 +0700

    config eslintrc

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -7,8 +7,8 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "nuxt-property-decorator";
-import PriceRangePicker from "./PriceRangePicker.vue";
+import { Vue, Component } from 'nuxt-property-decorator';
+import PriceRangePicker from './PriceRangePicker.vue';
 
 @Component({
   components: { PriceRangePicker },
